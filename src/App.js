addf8ee8a08583de71ccfe2e469480760093d369
majapiszczek <piszczekmaja33@gmail.com>
2023-02-21 09:27:50 +0100

    Added a link to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <p>hi, i'm making weather app here</p>
+      <footer>
+        Coded by Maja Piszczek,{" "}
+        <a href="https://github.com/majapiszczek/react-weather" target="_blank">
+          open source
+        </a>
+        .
+      </footer>
     </div>
   );
 }
